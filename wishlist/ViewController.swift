8677f25f8303826ea16162372d1bb3830e0d0286
Jerilyn Gonçalves <jerilyngoncalves@gmail.com>
2020-08-28 20:48:24 +0100

    Update: Xcode project initialization

---
+++ wishlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wishlist
+//
+//  Created by Jerilyn Gonçalves on 28/08/2020.
+//  Copyright © 2020 Jerilyn Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
